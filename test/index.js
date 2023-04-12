f8eb9104a191af4e7f4d1b92eee108022443e007
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2023-04-13 02:39:55 +0300

    Add test

---
+++ test/index.js
@@ -0,0 +1,29 @@
+import { FunctionalObject, makeClassProxy } from "../src/functional-object.js";
+
+class ContainerClass extends FunctionalObject {
+  constructor(...instanceArgs) {
+    super((...callArgs) => { this.callArgs = callArgs; return this; }); // arrow function, no prototype object created
+    this.instanceArgs = instanceArgs;
+  }
+  get content() {
+    return { instanceArgs: this.instanceArgs, callArgs: this.callArgs };
+  }
+}
+
+const Container = makeClassProxy(ContainerClass, {
+  apply(target, thisArg, argumentsList) {
+    return target(...argumentsList);
+  }
+});
+
+const container = Container('a', 'b');
+console.log('container: ', container);
+
+console.log(`container('c', 'd').content: `, container('c', 'd').content);
+console.log(`container: `, container);
+console.log('container instanceof Container: ', container instanceof Container);
+console.log('container instanceof ContainerClass: ', container instanceof ContainerClass);
+console.log('container instanceof FunctionalObject: ', container instanceof FunctionalObject);
+console.log('container instanceof Function: ', container instanceof Function);
+
+console.log('container.content: ', container.content);
